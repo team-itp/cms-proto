fe5ff0ae11a99302654f719f23df03e6f91c1af2
Yuto Nagano <chamelion@hotmail.co.jp>
2018-04-05 00:28:32 +0900

    フロントエンドに Vue が使用できるように変更
    https://github.com/Microsoft/TypeScript-Vue-Starter を参考に

---
+++ client/config/webpack/webpack.main.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+    output: {
+        path: path.resolve(__dirname, '../../dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                enforce: 'pre',
+                loader: 'tslint-loader',
+                options: {
+                    typeCheck: true,
+                    emitErrors: true
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx', '.jsx', '.json']
+    },
+    node: {
+        __dirname: false
+    },
+}
