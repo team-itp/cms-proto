745f27f263ac62d843513e7c7866aff4822fc003
Yuto Nagano <chamelion@hotmail.co.jp>
2018-04-04 21:33:22 +0900

    webpack を使用してコンパイルするように変更

---
+++ client/webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const commonConfig = {
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                enforce: 'pre',
+                loader: 'tslint-loader',
+                options: {
+                    typeCheck: true,
+                    emitErrors: true
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.tsx', '.jsx', '.json']
+    },
+    node: {
+        __dirname: false
+    },
+}
+
+module.exports = [
+    Object.assign(
+        {
+            target: 'electron-main',
+            entry: { main: './src/main.ts' }
+        },
+        commonConfig),
+    Object.assign(
+        {
+            target: 'electron-renderer',
+            entry: { renderer: './src/renderer.ts' },
+            plugins: [new HtmlWebpackPlugin()]
+        },
+        commonConfig)
+]
