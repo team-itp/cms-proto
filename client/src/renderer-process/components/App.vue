61f8b7837252971aa02b8ae7539618d30862e8b5
Yuto Nagano <chamelion@hotmail.co.jp>
2018-04-07 23:03:51 +0900

    ファイルの一覧表示・グリッド表示の切り替え

--- client/src/renderer-process/components/App.vue
+++ client/src/renderer-process/components/App.vue
@@ -1,5 +1,4 @@
 <!-- src/renderer-process/components/App.vue -->
-<!-- This is an alternative way to define the Hello component using decorators -->
 <template>
     <!-- Always shows a header, even in smaller screens. -->
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
@@ -11,50 +10,37 @@
                 <div class="mdl-layout-spacer"></div>
                 <!-- Navigation. We hide it in small screens. -->
                 <nav class="mdl-navigation">
-                    <a class="mdl-navigation__link" href=""><i class="material-icons">view_list</i></a>
+                    <a class="mdl-navigation__link" @click="toggleFileListView"><i class="material-icons">{{menuViewModeIcon}}</i></a>
                 </nav>
             </div>
         </header>
         <main class="mdl-layout__content">
             <div class="page-content">
-              <div class="mdl-grid">
-                <div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone">
-                  <selectable-card></selectable-card>
-                </div>
-                <div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone">
-                  <selectable-card></selectable-card>
-                </div>
-                <div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone">
-                  <selectable-card></selectable-card>
-                </div>
-                <div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone">
-                  <selectable-card></selectable-card>
-                </div>
-                <div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone">
-                  <selectable-card></selectable-card>
-                </div>
-                <div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone">
-                  <selectable-card></selectable-card>
-                </div>
-                <div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone">
-                  <selectable-card></selectable-card>
-                </div>
-              </div>
+                <file-list v-bind:view-mode="viewMode"></file-list>
             </div>
         </main>
     </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
-import SelectableCard from './SelectableCard.vue'
+import { Vue, Component } from "vue-property-decorator";
+import FileList from "./FileList.vue";
 
 @Component({
   components: {
-    SelectableCard
+    FileList
   }
 })
 export default class App extends Vue {
+  viewMode: 'list' | 'grid' = 'list'
+
+  get menuViewModeIcon() : string {
+    return this.viewMode === 'list' ? 'view_module' : 'view_list'
+  }
+
+  toggleFileListView() {
+    this.viewMode = (this.viewMode === 'list' ? 'grid' : 'list')
+  }
 }
 </script>
 
