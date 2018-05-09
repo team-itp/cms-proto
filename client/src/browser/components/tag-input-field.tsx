d0a4505f8a57a2670321ec77f05334b058e954e2
Yuto Nagano <chamelion@hotmail.co.jp>
2018-05-09 23:21:51 +0900

    タグの入力コンポーネントを追加

---
+++ client/src/browser/components/tag-input-field.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react'
+import Popover from 'material-ui/Popover'
+import Grow from 'material-ui/transitions/Grow'
+import Paper from 'material-ui/Paper'
+import { MenuItem } from 'material-ui/Menu'
+import TextField from 'material-ui/TextField'
+import { Tag } from '../../common/wp-api'
+
+const suggestions = [
+  { name: 'どこかの現場1', slug: 'docoka1' },
+  { name: 'どこかの現場2', slug: 'docoka2' }
+]
+
+interface TagInputProps {
+  label: string
+  onTagSelect: (tag: Tag) => void
+}
+
+interface TagInputState {
+  capture: boolean
+  open: boolean
+  value?: string
+  suggestion?: Tag[]
+}
+
+class TagInput extends React.Component<TagInputProps, TagInputState> {
+  private inputEl?: HTMLInputElement
+
+  constructor(props: TagInputProps) {
+    super(props)
+    this.state = {
+      open: false,
+      capture: false
+    }
+    this.handleClose = this.handleClose.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleBlur = this.handleBlur.bind(this)
+    this.handleSelect = this.handleSelect.bind(this)
+  }
+
+  handleClose() {
+    this.setState({ open: false })
+  }
+
+  handleChange(event: any) {
+    const value = event.target.value
+    this.setState({
+      value: value
+    })
+    if (value) {
+      setTimeout(() => {
+        if (this.state.value === value) {
+          const suggestion = suggestions.filter(v => RegExp(value).test(v.name))
+          if (suggestion.length) {
+            this.setState({ open: true, suggestion: suggestion })
+          } else {
+            this.setState({ open: false, suggestion: suggestion })
+          }
+        }
+      }, 300)
+    } else {
+      this.setState({ open: false, suggestion: [] })
+    }
+  }
+
+  handleBlur() {
+    this.setState({ open: false })
+  }
+
+  handleSelect(value: Tag): () => void {
+    return () => {
+      if (this.inputEl) {
+        this.inputEl.value = ''
+      }
+      this.setState({ open: false, value: '' })
+      this.props.onTagSelect.call(this, value)
+    }
+  }
+
+  render() {
+    const { label } = this.props
+    const { open, value, suggestion } = this.state
+
+    return (
+      <div>
+        <TextField inputRef={node => this.inputEl = node} label={label} value={value} onChange={this.handleChange} />
+        <Popover open={open} anchorEl={this.inputEl} anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }} disableAutoFocus style={{ top: 8 }}>
+          <Grow in={open}>
+            <Paper>
+              {open && suggestion!.map(v => {
+                return <MenuItem onClick={this.handleSelect(v)}>{v.name}</MenuItem>
+              })}
+            </Paper>
+          </Grow>
+        </Popover>
+      </div>
+    )
+  }
+}
+
+export default TagInput
--- client/src/browser/components/tag-input.tsx
+++ client/src/browser/components/tag-input.tsx
@@ -1,38 +1,80 @@
 import * as React from 'react'
+import Chip from 'material-ui/Chip'
 import { FormControl, FormLabel } from 'material-ui/Form'
 import ExpansionPanel, { ExpansionPanelSummary, ExpansionPanelDetails } from 'material-ui/ExpansionPanel'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import Typography from 'material-ui/Typography'
-import TextField from 'material-ui/TextField'
 import { Tag } from '../../common/wp-api'
+import TagInputField from './tag-input-field'
+import TagLabel from './tag-label'
 
 interface TagInputProps {
   displayName: string
+  required?: boolean
 }
 
 interface TagInputState {
-  selectedList: Tag[]
+  selectedTags: Tag[]
 }
 
 class TagInput extends React.Component<TagInputProps, TagInputState> {
   constructor(props: TagInputProps) {
     super(props)
     this.state = {
-      selectedList: []
+      selectedTags: []
+    }
+    this.handleTagSelect = this.handleTagSelect.bind(this)
+  }
+
+  handleTagDelete(value: Tag): () => void {
+    return () => {
+      let selectedTags = this.state.selectedTags.filter(v => v.slug !== value.slug)
+      this.setState({
+        selectedTags: selectedTags
+      })
     }
   }
 
+  handleTagSelect(value: Tag): void {
+    if (this.state.selectedTags.filter(v => v.slug === value.slug).length === 0) {
+      const selectedTags = this.state.selectedTags.slice()
+      selectedTags.push(value)
+      this.setState({
+        selectedTags: selectedTags.sort((a, b) => a.name.localeCompare(b.name))
+      })
+    }
+  }
+
+  renderTags(renderTags: Tag[]) {
+    return (
+      renderTags.map(v => {
+        return (
+          <Chip label={v.name} onDelete={this.handleTagDelete(v)} style={{ margin: 8, marginLeft: 0 }} />
+        )
+      })
+    )
+  }
+
   render() {
+    const { selectedTags } = this.state
     return (
       <ExpansionPanel defaultExpanded>
         <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
           <FormLabel component='label'>{this.props.displayName}</FormLabel>
-          <Typography style={{ lineHeight: 1, fontSize: '1rem', position: 'absolute', left: '7em' }}>{this.state.selectedList ? this.state.selectedList.map(v => v.name).join(', ') : ''}</Typography>
+          <TagLabel tags={selectedTags} />
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
-          <FormControl component='fieldset'>
-            <TextField type='text' label='タグ名' />
-          </FormControl>
+          <div>
+            <FormControl component='fieldset' required={this.props.required} style={{ width: '100%', margin: 0 }}>
+              <TagInputField label='タグ' onTagSelect={this.handleTagSelect} />
+            </FormControl>
+            <div style={{
+              display: 'flex',
+              justifyContent: 'left',
+              flexWrap: 'wrap'
+            }}>
+              {this.renderTags(selectedTags)}
+            </div>
+          </div>
         </ExpansionPanelDetails>
       </ExpansionPanel>
     )
